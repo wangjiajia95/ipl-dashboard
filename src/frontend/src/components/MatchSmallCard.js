a2399f3c01de9b94216349c932888b5c168f5e58
Jiajia Wang <grace.wangjiajia@gmail.com>
2022-01-24 12:06:58 -0800

    styled the team page

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,17 +1,22 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
+import "./MatchSmallCard.scss";
+
+
 
 export const MatchSmallCard = ({teamName, match}) => {
   const otherTeam = match.team1 === teamName? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
+
   return (
-    <div className="MatchSmallCard">
-      <h3>vs 
-        <Link to={otherTeamRoute}> {otherTeam} </Link>
-      </h3>
+    <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
       
-        
-      <p>{match.matchWinner} won by {match.result.Margin} {match.result}</p>
+      <span className='vs'>vs</span>
+          
+          <h1> <Link to={otherTeamRoute}> {otherTeam} </Link></h1>
+      
+      <p className="match-result">{match.matchWinner} won by {match.result.Margin} {match.result}</p>
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,9 @@ import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
+import './TeamPage.scss';
+
 
 export const TeamPage = () => {
   // not return undefine matches: []
@@ -30,12 +33,34 @@ export const TeamPage = () => {
 
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
+    <div className="teamName-name-section">  
+      <h1 className="teamName-name">{team.teamName}</h1>
+    </div>
+    <div className="win-loss-section">Wins / Losses
+      <PieChart
+        data={[
+          { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+          { title: 'Wins', value: team.totalWins, color: '#4da375' },
+          
+        ]}
+      />
+    
+    
+    </div>
+    <div className="match-detail-section">
+    <h3>Latest Matches</h3>
       {/* first page showing */}
       <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/> 
+    </div>
     {/* remove first use slice */}
       {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
       
+      <div className="more-link">
+        <a href="#">More > </a>
+
+      </div>
+
+
     </div>
   );   
 
